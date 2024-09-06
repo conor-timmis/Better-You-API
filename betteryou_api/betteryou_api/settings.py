634f44798b2c01d70a8dedbbfba04d9d82342f1b
Conor Timmis <mctto@protonmail.com>
2024-09-06 20:14:54 +0000

    Install Axios and Test API Connection

--- betteryou_api/betteryou_api/settings.py
+++ betteryou_api/betteryou_api/settings.py
@@ -49,7 +49,7 @@ SECRET_KEY = 'django-insecure-y32hzm&ufo(047_g#ty0pem&mkvf_jrn#-18px)7(k(3elap16
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-conortimmis-betteryou-aduglnzndgu.ws.codeinstitute-ide.net']
 
 
 # Application definition
--- betteryou_api/betteryou_api/urls.py
+++ betteryou_api/betteryou_api/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import test_connection
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontend.urls')),
+    path('api/test-connection/', test_connection),
 ]
