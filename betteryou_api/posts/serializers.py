992419aa77df6966dd7b2ac8ea76e8116c7b1008
Conor Timmis <mctto@protonmail.com>
2024-09-08 19:42:28 +0000

    Add image validation to Posts App Serializers

--- betteryou_api/posts/serializers.py
+++ betteryou_api/posts/serializers.py
@@ -7,6 +7,20 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise  serializers.ValidationError(
+                'Image size larger than 2MB!'
+            )
+            if value.image.width > 4096:
+                raise serializers.ValidationError(
+                    'Image width larger than 4096px'
+                )
+            if value.image.height > 4096:
+                raise serializers.ValidationError(
+                    'Image height larger than 4096px!'
+                )
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -17,5 +31,5 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image'
+            'title', 'content', 'image', 'image_filter'
         ]
