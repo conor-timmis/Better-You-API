1b32b1c8fd0b9c4d430d555578221323e2077f37
Conor Timmis <mctto@protonmail.com>
2024-09-08 16:37:22 +0000

    Delete and Remove TestConnection Component

--- betteryou/src/App.js
+++ betteryou/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import TestConnection from './components/TestConnection';
 
 function App() {
     return (
         <div className="App">
-            <TestConnection />
+            
         </div>
     );
 }
