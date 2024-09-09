885f211cc46dfa02ef8fb3764e422be145cf183e
Conor Timmis <mctto@protonmail.com>
2024-09-09 15:29:23 +0000

    Add CommentDetailSerializer with read-only Post

--- betteryou_api/comments/serializers.py
+++ betteryou_api/comments/serializers.py
@@ -22,3 +22,11 @@ class CommentSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
             'post', 'created_at', 'updated_at', 'content'
         ]
+
+
+class CommentDetailSerializer(CommentSerializer):
+    """
+    Handles Comment model in Detail view
+    Post is read-only to avoid setting it on each update
+    """
+    post = serializers.ReadOnlyField(source='post.id')
