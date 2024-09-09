422ab4bfbc886e93de9a312c94a4f064101f81bd
Conor Timmis <mctto@protonmail.com>
2024-09-09 14:00:39 +0000

    Add PostDetailViewTests for Post model

--- betteryou_api/posts/tests.py
+++ betteryou_api/posts/tests.py
@@ -26,3 +26,36 @@ class PostListViewTests(APITestCase):
     def test_user_not_logged_in_cant_create_post(self):
         response = self.client.post('/posts/', {'title': 'a title'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class PostDetailViewTests(APITestCase):
+    def setUp(self):
+        conor = User.objects.create_user(username='conor', password='pass')
+        TJ = User.objects.create_user(username='TJ', password='pass')
+        Post.objects.create(
+            owner=conor, title='a title', content='conors content'
+        )
+        Post.objects.create(
+            owner=TJ, title='another title', content='TJs content'
+        )
+
+    def test_can_retrieve_post_using_valid_id(self):
+        response = self.client.get('/posts/1/')
+        self.assertEqual(response.data['title'], 'a title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cant_retrieve_post_using_invalid_id(self):
+        response = self.client.get('/posts/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_own_post(self):
+        self.client.login(username='conor', password='pass')
+        response = self.client.put('/posts/1/', {'title': 'a new title'})
+        post = Post.objects.filter(pk=1).first()
+        self.assertEqual(post.title, 'a new title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cant_update_another_users_post(self):
+        self.client.login(username='conor', password='pass')
+        response = self.client.put('/posts/2/', {'title': 'a new title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
