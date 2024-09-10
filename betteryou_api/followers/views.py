cedb5a034bd553cab4be404cf8e75b9d0b272803
Conor Timmis <mctto@protonmail.com>
2024-09-10 17:46:01 +0000

    Create FollowerList View for Followers App

--- betteryou_api/followers/views.py
+++ betteryou_api/followers/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from betteryou_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    """
+    List all followers (users following other users).
+    Create a follower (follow a user if logged in).
+    Perform_create: link the logged-in user with the follower.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
