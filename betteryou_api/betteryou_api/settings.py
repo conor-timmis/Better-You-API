8002bae83661ba7dd4752802b20bfd9ce1dd22cb
Conor Timmis <mctto@protonmail.com>
2024-09-15 16:01:10 +0000

    Import dj-database-url & Database to Settings

--- betteryou_api/betteryou_api/settings.py
+++ betteryou_api/betteryou_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -152,10 +153,7 @@ WSGI_APPLICATION = 'betteryou_api.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
