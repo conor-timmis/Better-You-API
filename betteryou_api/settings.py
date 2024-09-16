a8b89e9c5457e41357f9d4928bb77d0a811baa4e
Conor Timmis <mctto@protonmail.com>
2024-09-16 18:28:02 +0000

    Set Debug to False unless in Development

--- betteryou_api/settings.py
+++ betteryou_api/settings.py
@@ -69,7 +69,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = 'django-insecure-y32hzm&ufo(047_g#ty0pem&mkvf_jrn#-18px)7(k(3elap16'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = ['8000-conortimmis-betteryou-n5cigzi8keh.ws.codeinstitute-ide.net', 'better-you-ec0aa381f182.herokuapp.com']
 
