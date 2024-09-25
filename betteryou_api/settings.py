64265fdaa742adac09ff30a31ef694cc32610ea5
Conor Timmis <mctto@protonmail.com>
2024-09-25 13:08:03 +0000

    Add rest_framework_simplejwt to INSTALLED_APPS

--- betteryou_api/settings.py
+++ betteryou_api/settings.py
@@ -41,13 +41,14 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [(
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
-        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+        else 'rest_framework_simplejwt.authentication.JWTAuthentication'
     )],
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DATETIME_FORMAT': '%d %b %Y',
 }
+
 if 'DEV' not in os.environ:
     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
         'rest_framework.renderers.JSONRenderer',
@@ -90,6 +91,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'rest_framework',
+    'rest_framework_simplejwt',
     'django_filters',
     'rest_framework.authtoken',
     'django.contrib.sites',
