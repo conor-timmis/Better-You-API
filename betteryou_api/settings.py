d2f56cbd4037a6a4f077c281ef3f3d3ba454b5a7
Conor Timmis <mctto@protonmail.com>
2024-09-29 10:52:15 +0000

    Remove account.middleware

--- betteryou_api/settings.py
+++ betteryou_api/settings.py
@@ -140,7 +140,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'betteryou_api.urls'
