53f2e7d2e097e37c042993592d85525587452a89
Conor Timmis <mctto@protonmail.com>
2024-10-03 20:06:27 +0000

    Update url pattern to include UserDetailView

--- betteryou_api/urls.py
+++ betteryou_api/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from .views import home_route, logout_route
+from .views import home_route, logout_route, UserDetailView
 
 urlpatterns = [
     path('', home_route),
@@ -10,6 +10,7 @@ urlpatterns = [
     path('api/dj-rest-auth/logout/', logout_route),
     path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/dj-rest-auth/user/', UserDetailView.as_view(), name='user-detail'),
     path('api/profiles/', include('profiles.urls')),
     path('api/posts/', include('posts.urls')),
     path('api/comments/', include('comments.urls')),
