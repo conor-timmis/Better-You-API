deed5a11ec3cf7dca2f67490991706bd2e0c0bd1
Conor Timmis <mctto@protonmail.com>
2024-10-13 13:24:04 +0000

    Add refresh to urlpatterns

--- betteryou_api/urls.py
+++ betteryou_api/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from .views import home_route, logout_route, UserDetailView
+from .views import home_route, logout_route, UserDetailView, TokenRefreshView
 
 urlpatterns = [
     path('', home_route),
@@ -11,6 +11,7 @@ urlpatterns = [
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('dj-rest-auth/user/', UserDetailView.as_view(), name='user-detail'),
+    path('dj-rest-auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
     path('', include('comments.urls')),
