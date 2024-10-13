fe4daffbf952ef333a051bc28f0f76bc74a29d55
Conor Timmis <mctto@protonmail.com>
2024-10-13 13:23:55 +0000

    Add TokenRefreshView to Main app views

--- betteryou_api/views.py
+++ betteryou_api/views.py
@@ -3,15 +3,14 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 from .settings import (
     JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
     JWT_AUTH_SECURE
 )
 
 def clear_jwt_cookies(response):
-    """
-    Helper function to clear JWT authentication cookies.
-    """
+    """ Helper function to clear JWT authentication cookies. """
     cookies = [
         (JWT_AUTH_COOKIE, ''),
         (JWT_AUTH_REFRESH_COOKIE, '')
@@ -30,27 +29,21 @@ def clear_jwt_cookies(response):
 
 @api_view()
 def home_route(request):
-    """
-    Route for home page of API.
-    """
+    """ Route for home page of API. """
     return Response({
         "message": "Hello! This is The Better You DRF API"
     })
 
 @api_view(['POST'])
 def logout_route(request):
-    """
-    Route for logout of API.
-    """
+    """ Route for logout of API. """
     response = Response({
         "message": "You have been logged out."
     })
     return clear_jwt_cookies(response)
 
 class UserDetailView(APIView):
-    """
-    View to retrieve details of the authenticated user.
-    """
+    """ View to retrieve details of the authenticated user. """
     def get(self, request):
         user = request.user
         if user.is_authenticated:
@@ -63,3 +56,19 @@ class UserDetailView(APIView):
             return Response(data, status=status.HTTP_200_OK)
         else:
             return Response({'error': 'User not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
+
+class TokenRefreshView(APIView):
+    """ View to handle token refresh. """
+    def post(self, request, *args, **kwargs):
+        try:
+            refresh_token = request.data.get('refresh')
+            if not refresh_token:
+                return Response({'error': 'Refresh token is missing'}, status=400)
+
+            token = RefreshToken(refresh_token)
+            new_token = {
+                'access': str(token.access_token)
+            }
+            return Response(new_token, status=200)
+        except Exception as e:
+            return Response({'error': 'Server Error'}, status=500)
