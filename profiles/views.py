2f86005aa8ebf194bebf212ef92bf5195ca4ec86
Conor Timmis <mctto@protonmail.com>
2024-10-17 17:55:33 +0000

    Add filters and count fields to ProfileList

--- profiles/views.py
+++ profiles/views.py
@@ -11,23 +11,29 @@ class ProfileList(generics.ListAPIView):
     No Create view (post method), as profile creation handled by django signals
     """
     queryset = Profile.objects.annotate(
-        posts_count=Count('owner__post', distinct=True),
-        followers_count=Count('owner__followed', distinct=True),
-        following_count=Count('owner__following', distinct=True)
+        followers_total=Count('owner__followed', distinct=True),
+        following_total=Count('owner__following', distinct=True),
+        posts_total=Count('owner__post', distinct=True)
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+
+        # displays profile results that are following
+        'owner__following__followed__profile',
+        # displays profile results that are followed
+        'owner__followed__owner__profile',
     ]
     ordering_fields = [
-        'posts_count',
-        'followers_count',
-        'following_count',
+        'posts_total',
         'owner__following__created_at',
         'owner__followed__created_at',
+        'followers_total',
+        'following_total',
     ]
-    filterset_fields = ['owner__followed__owner__profile']
-
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
